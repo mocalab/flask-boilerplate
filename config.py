18289d874c122384360ac9de33d82def81d0af90
yusuf ozturk <yozturk@mocala.com>
2025-12-28 17:33:11 -0800

    update

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ DEBUG = True
 
 # Secret key for session management. You can generate random strings here:
 # https://randomkeygen.com/
-SECRET_KEY = 'my precious'
+#SECRET_KEY = 'aed691be502fc12e1babcb0c2d1841e0e916be8f7910b2732e265b2d43b03612'
+SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") 
 
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
